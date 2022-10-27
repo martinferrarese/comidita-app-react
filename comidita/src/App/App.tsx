ea93c728b7aba2372ce789e4ad8335be117857b8
Martín Ferrarese <martin.ferrarese@gmail.com>
2022-10-26 21:15:41 -0300

    Crea el componente Calendar

--- comidita/src/App/App.tsx
+++ comidita/src/App/App.tsx
@@ -1,37 +1,12 @@
 import './App.css';
+import Calendar from './Calendar/Calendar';
 import Header from './Header/Header';
 
 const App = () => {
   return (
     <div className='App'>
       <Header>Comidita</Header>
-      <div className='Calendar'>
-        <div className='DayOfTheWeek'>
-          <p><b>Lunes</b></p>
-          <p>Almuerzo</p>
-          <p>Cena</p>
-        </div>
-        <div className='DayOfTheWeek'>
-          <p><b>Martes</b></p>
-          <p>Almuerzo</p>
-          <p>Cena</p>
-        </div>
-        <div className='DayOfTheWeek'>
-          <p><b>Miércoles</b></p>
-          <p>Almuerzo</p>
-          <p>Cena</p>
-        </div>
-        <div className='DayOfTheWeek'>
-          <p><b>Jueves</b></p>
-          <p>Almuerzo</p>
-          <p>Cena</p>
-        </div>
-        <div className='DayOfTheWeek'>
-          <p><b>Viernes</b></p>
-          <p>Almuerzo</p>
-          <p>Cena</p>
-        </div>
-      </div>
+      <Calendar></Calendar>
     </div>
   );
 }
---
+++ comidita/src/App/Calendar/Calendar.tsx
@@ -0,0 +1,34 @@
+const Calendar = () => {
+    return (
+        <div className='Calendar'>
+            <div className='DayOfTheWeek'>
+                <p><b>Lunes</b></p>
+                <p>Almuerzo</p>
+                <p>Cena</p>
+            </div>
+            <div className='DayOfTheWeek'>
+                <p><b>Martes</b></p>
+                <p>Almuerzo</p>
+                <p>Cena</p>
+            </div>
+            <div className='DayOfTheWeek'>
+                <p><b>Miércoles</b></p>
+                <p>Almuerzo</p>
+                <p>Cena</p>
+            </div>
+            <div className='DayOfTheWeek'>
+                <p><b>Jueves</b></p>
+                <p>Almuerzo</p>
+                <p>Cena</p>
+            </div>
+            <div className='DayOfTheWeek'>
+                <p><b>Viernes</b></p>
+                <p>Almuerzo</p>
+                <p>Cena</p>
+            </div>
+        </div>
+    )
+
+}
+
+export default Calendar;
