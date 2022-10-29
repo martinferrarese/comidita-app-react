a7f90024ead3fb2943e0e39cb00ca2615e97dbf0
Martín Ferrarese <martin.ferrarese@gmail.com>
2022-10-29 20:56:13 -0300

    Permite agregar almuerzos y cenas en los distintos días

--- comidita/src/App/Calendar/Day/Day.tsx
+++ comidita/src/App/Calendar/Day/Day.tsx
@@ -1,13 +1,30 @@
+import { useState } from "react"
+
 type DayType = {
     day: string
 }
 
 const Day = ({ day }: DayType) => {
+    const [lunch, setLunch] = useState<string>("");
+    const [dinner, setDinner] = useState<string>("");
+
     return (
         <div className='DayOfTheWeek'>
-            <p><b>{day}</b></p>
+            <p>
+                <b>{day}</b>
+            </p>
             <p>Almuerzo</p>
+            <input
+                name="lunch"
+                value={lunch}
+                onChange={(e) => setLunch(e.target.value)}
+            />
             <p>Cena</p>
+            <input
+                name="dinner"
+                value={dinner}
+                onChange={(e) => setDinner(e.target.value)}
+            />
         </div>
     )
 }
