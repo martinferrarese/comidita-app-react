ffbb86156396dc14561dba1aa953baf44f058d2e
Martín Ferrarese <martin.ferrarese@gmail.com>
2022-10-29 21:16:35 -0300

    Guarda en localStorage los almuerzos

--- comidita/src/App/Calendar/Day/Day.tsx
+++ comidita/src/App/Calendar/Day/Day.tsx
@@ -8,13 +8,26 @@ const Day = ({ day }: DayType) => {
     const [lunch, setLunch] = useState<string>("");
     const [dinner, setDinner] = useState<string>("");
 
+    const saveLunch = (newLunch: string) => {
+        localStorage.setItem("lunch", newLunch);
+        setLunch(newLunch);
+    }
+
     const saveDinner = (newDinner: string) => {
         localStorage.setItem("dinner", newDinner);
         setDinner(newDinner);
     }
 
     useEffect(() => {
-        const localStorageDinnerValue = localStorage.getItem("dinner")
+        const localStorageLunchValue = localStorage.getItem("lunch");
+        const localStorageDinnerValue = localStorage.getItem("dinner");
+
+        if (!localStorageLunchValue) {
+            localStorage.setItem("lunch", "N/A");
+        } else {
+            setLunch(localStorageLunchValue);
+        }
+
         if (!localStorageDinnerValue) {
             localStorage.setItem("dinner", "N/A");
         } else {
@@ -31,7 +44,7 @@ const Day = ({ day }: DayType) => {
             <input
                 name="lunch"
                 value={lunch}
-                onChange={(e) => setLunch(e.target.value)}
+                onChange={(e) => saveLunch(e.target.value)}
             />
             <p>Cena</p>
             <input
